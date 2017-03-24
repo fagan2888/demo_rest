bc1a3beac9de5f0145044137b9e9d7e23bcacf78
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2017-03-24 17:55:52 +0100

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='demo_rest',
+    version="v0.0.1",
+    author='Thomas Schmelzer',
+    author_email='thomas.schmelzer@gmail.com'
+)
